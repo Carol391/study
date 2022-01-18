fcb513a3a1bcf1315c0a3cc6d1efe586870fdcf3
Huong. Tran Thi Thu - CMC Global DU1.12 <ttthuong4@cmcglobal.vn>
2022-01-18 17:26:49 +0700

    init

---
+++ src/main/java/com/example/springSercurity/entity/Product.java
@@ -0,0 +1,17 @@
+package com.example.springSercurity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    private long id;
+    private String name;
+    private float price;
+
+}
